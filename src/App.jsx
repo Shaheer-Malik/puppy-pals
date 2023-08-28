ca8338a4483a1526bf1fadf70c6c84638a316321
Shaheer-Malik <shaheerm1223@gmail.com>
2023-08-28 14:56:56 -0400

    finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,39 @@
+import {puppyList} from './data.js'
 import './App.css'
+import { useState } from 'react'
 
 function App() {
+  
+  const [puppies, setPuppies] = useState(puppyList);
+  
+  // console.log(puppies);
+
+  const [featPupId, setFeatPupId] = useState(null);
+
+  const featuredPup = puppies.find((pup) => pup.id === featPupId);
+  console.log(featuredPup);
 
   return (
     <>
-      <div>
-
+      <div className="App">
+        {
+          puppies.map((puppy) => {
+            return <p onClick={() => {setFeatPupId(puppy.id)}} key={puppy.id}>{puppy.name}</p>;
+          })
+        }
+        {featPupId && (
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
       </div>
 
     </>
-  )
+  );
 }
 
 export default App
